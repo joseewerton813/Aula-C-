cdae7774ca3d83cacd792f31af0d31d3966abe2a
JOSE EWERTON DA SILVA <jose.esilva47@senacsp.edu.br>
2025-10-22 21:55:19 -0300

    inicializando as variáveis

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var n = 10;
+int x = 1;
+double preco = 10.50;
+char inicial = 'M';
+String nome = "Ewerton";
+bool status = true;
+int idade = 27;
+
+Console.WriteLine($"Status: {status}");
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Idade: {idade}");
